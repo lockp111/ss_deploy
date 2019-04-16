65efbfa082d17f7eaa71b43a21b6402d7409d5ba
harman <lockp111@protonmail.com>
2019-04-16 19:04:05 +0800

    refactor: remove mtproto and add socks5

--- install.sh
+++ install.sh
@@ -2,35 +2,15 @@
 
 runDocker (){
     if [ ! -n "$1" ]; then
-        echo "Usage: $0 <mt secret> <pwd>" && \
+        echo "Usage: $0 <pwd>" && \
         exit
     fi
 
-    if [ ! -n "$2" ]; then
-        echo "Usage: $0 <mt secret> <pwd>" && \
-        exit
-    fi
-
-    docker run -d -p443:443 --name=mtproto-proxy \
-        --restart=always \
-        -v proxy-config:/data \
-        -e SECRET=$1 \
-        --name mtproto \
-        --network host \
-        telegrammessenger/proxy:latest
-
-    docker run --privileged -d \
-        -p 989:989/tcp \
-        -p 989:989/udp \
-        --name ss2 \
-        -e PASSWORD=$2 \
-        -e PROTO=AES_256_GCM \
-        -e PORT=989 \
-        lockp111/ss2:latest
+    export PASSWORD=$1 && docker-compose up -d
 }
 
 if hash docker 2>/dev/null; then 
-  runDocker $1 $2
-else 
-  git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh
+  runDocker() $1
+else
+  git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh && runDocker() $1
 fi
