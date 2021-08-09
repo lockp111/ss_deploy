bdaa67ff7e9cdce342406af5bd6723281fe587b4
Harman <lockp111@protonmail.com>
2021-08-09 17:36:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-runDocker (){
-    if [ ! -n "$1" ]; then
-        echo "Usage: $0 <pwd>" && \
-        exit
-    fi
+runDocker() {
+  if [ ! -n "$1" ]; then
+    echo "Usage: $0 <pwd>" &&
+      exit
+  fi
 
-    PASSWORD=$1 docker-compose up -d
+  PASSWORD=$1 docker-compose up -d
 }
 
-if hash docker 2>/dev/null; then 
+if hash docker 2>/dev/null; then
   runDocker $1
 else
   git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh && runDocker $1
