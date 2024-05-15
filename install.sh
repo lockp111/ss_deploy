9d7d09c25aaa0c123bbe13d40fd7e9307574b1cc
Harman <lockp111@protonmail.com>
2024-05-15 09:39:55 +0800

    update

--- install.sh
+++ install.sh
@@ -13,4 +13,9 @@ if hash docker 2>/dev/null; then
   runDocker $1
 else
   git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh && runDocker $1
+  apt install -y debian-keyring debian-archive-keyring apt-transport-https
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+  apt update -y
+  apt install -y caddy
 fi
