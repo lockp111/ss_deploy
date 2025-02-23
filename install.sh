6630c503e03e19f19aea11d0984b668cb7f8ada9
Harman.P <lockp111@protonmail.com>
2025-02-23 18:32:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 runDocker() {
-  if [ ! -n "$1" ]; then
-    echo "Usage: $0 <pwd>" &&
-      exit
-  fi
-
-  PASSWORD=$1 docker compose up -d
+  docker compose up -d
 }
 
 if hash docker 2>/dev/null; then
-  runDocker $1
+  runDocker
 else
-  git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh && runDocker $1
   apt install -y debian-keyring debian-archive-keyring apt-transport-https
+  git clone https://github.com/lockp111/docker-install.git && sh ./docker-install/install.sh && runDocker
   curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
   curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
   apt update -y
